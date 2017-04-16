beb5b077d519ad31cbd982304f39e489f82b5cf5
The Metaist <metaist@metaist.com>
2017-04-16 11:49:09 -0400

    add: initial speaking

--- www/app/index.js
+++ www/app/index.js
@@ -1,6 +1,11 @@
 'use strict';
 
 function noop() {}
+function noopEvent(e) {
+  e.stopImmediatePropagation();
+  e.stopPropagation();
+  return false;
+}
 
 function bindEvents(thisArg, events) {
    Object.keys(events).forEach(function (selector) {
@@ -24,36 +29,78 @@ function f(name, params) {
   return name + '(' + params.join(', ') + ')';
 }
 
+var IS_CORDOVA = !!window.cordova;
+
 var app = {
   // options
-  prefs: null,
+  DATA_KEY: 'org.metaist.sayit.data',
+  store: null,
+  options: {
+    debug: true
+  },
 
   // internal
-  //
+  msg: '',
+
+  // DOM
+  // TODO
 
   init: function () {
     bindEvents(this, {
       'document': {'deviceready': this.ready},
-      'form input': {'change': this.change}
+      'form': {'submit': noopEvent},
+      'form input': {'change': this.change},
+      '#btn-play': {'click': this.play},
+      '#msg': {'change': this.change}
     });
+
+    if(!IS_CORDOVA) {
+      this.options.debug && console.log('NOT cordova');
+      bindEvents(this, {'window': {'load': this.ready}});
+    }
+
     return this;
   },
 
   ready: function () {
     // Store DOM nodes
-    // TODO
+    this.$msg = document.querySelector('#msg');
+
+    this.$msg.focus();
 
     // Grab preferences
-    // this.prefs = plugins.appPreferences;
-    this.start();
+    if(IS_CORDOVA) {
+      this.store = plugins.appPreferences;
+      this.store.fetch(this.DATA_KEY).then(function (data) {
+        this.options = data || this.options;
+        // TODO: update settings UI
+        this.render();
+      }.bind(this));
+    }
+
     return this;
   },
 
   change: function () {
+    this.msg = this.$msg.value;
+
+    if (IS_CORDOVA) {
+      this.store.store(noop, noop, this.DATA_KEY, this.options);
+    }//end if: options stored
     return this;
   },
 
   render: function () {
+    return this;
+  },
+
+  play: function () {
+    if (IS_CORDOVA) {
+      TTS.speak(this.msg);
+    } else {
+      var msg = new SpeechSynthesisUtterance(this.msg);
+      window.speechSynthesis.speak(msg);
+    }
     return this;
   }
 };
